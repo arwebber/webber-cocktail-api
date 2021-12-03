18c493d94c3ab84734da2f633521a1f232435e99
arwebber <andywebber96@gmail.com>
2021-12-03 14:38:05 -0600

    add get cocktail by ingredient service

--- src/services/cocktail/cocktail.service.js
+++ src/services/cocktail/cocktail.service.js
@@ -218,7 +218,56 @@ class CocktailService {
 
     // Lookup ingredient by ID
     // www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=552
+    async getCocktailsByIngredient(id) {
+        return new Promise(function(resolve, reject){
+            let url = `${COCKTAILDB_URI}/lookup.php?iid=${id}`;
 
+            let response = {}
+
+            // Options used by request
+            const options = {
+                'method': 'GET',
+                'url': url
+            };
+
+            // Get the drink response from cocktaildb
+            request(options, function (error, res) { 
+                // Set the response status code. If there was an error, set the code to 503
+                response.status = res ? res.statusCode : 503;
+
+                // Check if there was an error returning data from cocktaildb, log the error and resolve with the error.
+                if (error) {
+                    console.log(error);
+                    response.body =  {};
+                    response.errMsg = error.toString();
+                    return resolve(response);
+                } else if (res.body === null || res.body.trim() === '') {
+                    response.body = 'No data returned';
+                    return resolve(response)
+                }
+
+                // Set the response to 200 since there was no error.
+                response.status = res.statusCode;
+
+                // Parse the response as JSON.
+                let apiResponseBody = {};
+                try {
+                    // console.log('res body', res.body);
+                    apiResponseBody = JSON.parse(res.body);
+                    // console.log('parsed', apiResponseBody);
+                    response.body = {
+                        ...apiResponseBody
+                    }
+                } catch {
+                    response.body = {};
+                    response.errMsg = 'Unable to parse cocktaildb response.'
+                }
+
+                return resolve(response);
+            });
+        });
+    }
+    
     // Lookup a random cocktail
     // www.thecocktaildb.com/api/json/v1/1/random.php
 
