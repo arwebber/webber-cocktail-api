a4b12e0c9026a02c53c452862f7ad89237967690
arwebber <andywebber96@gmail.com>
2021-12-03 14:17:52 -0600

    add controllers file

---
+++ src/controllers/cocktail/cocktail.controller.js
@@ -0,0 +1,53 @@
+const { CocktailService } = require('../../services/');
+
+class CocktailController {
+	async getCocktailsByName(req, res, next) {
+		try {
+                  const cocktailService = new CocktailService()
+                  const { cocktailName } = req.params;
+                  const response = await cocktailService.getCocktailsByName(cocktailName);
+                  return res.json({response});
+            } 
+            catch (err) {
+                  return next(err);
+            }
+	}
+
+      async getCocktailsByFirstLetter(req, res, next) {
+		try {
+                  const cocktailService = new CocktailService()
+                  const { firstLetter } = req.params;
+                  const response = await cocktailService.getCocktailsByFirstLetter(firstLetter);
+                  return res.json({response});
+            } 
+            catch (err) {
+                  return next(err);
+            }
+	}
+
+      async getCocktailsByIngredient(req, res, next) {
+		try {
+                  const cocktailService = new CocktailService()
+                  const { ingredient } = req.params;
+                  const response = await cocktailService.getCocktailsByIngredient(ingredient);
+                  return res.json({response});
+            } 
+            catch (err) {
+                  return next(err);
+            }
+	}
+
+      async getCocktailById(req, res, next) {
+		try {
+                  const cocktailService = new CocktailService()
+                  const { id } = req.params;
+                  const response = await cocktailService.getCocktailById(id);
+                  return res.json({response});
+            } 
+            catch (err) {
+                  return next(err);
+            }
+	}
+}
+
+module.exports = CocktailController
